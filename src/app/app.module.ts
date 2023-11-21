3a59d5541f85d9b0ec06cbd12a8542c533c76c85
Monstro <nicolasmeireles1004@gmail.com>
2023-11-20 23:31:58 -0300

    feat: implementando layout de login.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Import PrimeNG modules
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { RippleModule } from 'primeng/ripple';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './modules/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule, // Add BrowserAnimationsModule
     ReactiveFormsModule, // Add ReactiveFormsModule
-    HttpClientModule // Add HttpClientModule
+    HttpClientModule, // Add HttpClientModule
+
+    // PrimeNG modules
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
+    RippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
